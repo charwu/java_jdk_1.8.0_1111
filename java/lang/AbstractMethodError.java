d6b3b3011e8a7f368819a2850f887b9be3bb6f79
吴小兵 <wuxiaobinwow@126.com>
2020-08-24 16:27:00 +0800

    提交：AbstractMethodError 中文译文

--- java/lang/AbstractMethodError.java
+++ java/lang/AbstractMethodError.java
@@ -32,6 +32,11 @@ package java.lang;
  * incompatibly changed since the currently executing method was last
  * compiled.
  *
+ * ### 译文:
+ *  当应用程序试图调用一个抽象方法时，抛出此错误。
+ *  通常由编译器捕获此错误；如果某个类的定义自当前执行方法最后一次编译以后作了不兼容的更改，
+ *  则此错误只可能在运行时发生
+ *
  * @author  unascribed
  * @since   JDK1.0
  */
@@ -41,6 +46,10 @@ class AbstractMethodError extends IncompatibleClassChangeError {
 
     /**
      * Constructs an <code>AbstractMethodError</code> with no detail  message.
+     *
+     * ### 译文:
+     *  构造不带详细消息的 AbstractMethodError。
+     *
      */
     public AbstractMethodError() {
         super();
@@ -49,6 +58,9 @@ class AbstractMethodError extends IncompatibleClassChangeError {
     /**
      * Constructs an <code>AbstractMethodError</code> with the specified
      * detail message.
+     *
+     * ### 译文:
+     *  构造带指定详细消息的 AbstractMethodError。
      *
      * @param   s   the detail message.
      */
